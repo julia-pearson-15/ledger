c6e7ac0f1008375885cc66dbec77427886b4dab6
Julia Pearson <julia.pearson@gmail.com>
2015-11-04 14:28:12 -0500

    loans and purses

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
 $(document).ready(function() {
   var $input = $('#input');
   var $logList = $('#log');
+  var purseBal = 1000;
+
+  var makeStringArray = function() {
+    var stringed = $input.val();
+    var words = stringed.split(' ');
+    return words;
+  }
+  // var checkIfLoan = function() {
+  //   if ($input.val().charAt(0)==='l') {
+  //     if ($input.val().charAt(0)==='o') {
+  //       if ($input.val().charAt(0)==='a') {
+  //         if ($input.val().charAt(0)==='n') {
+  //           return true;
+  //         }; 
+  //       };  
+  //     };  
+  //   };
+  // }
 
   var respond = function(question){
     var answer  = '';
     var newList = $('<li>');    
+    var questions = makeStringArray();
+    if(questions[0]==='purse'){
+      newList.addClass('action');
+      answer='ShylockBot pulls out his purse containing '+purseBal+' ducats';
+    }else if(questions[0]==='loan'){
+      newList.addClass('action');
+      answer='ShylockBot gives'+questions[1]+' '+questions[2]+' ducats';
+      purseBal-=questions[2];
+    }else{
+
+    }
     newList.text(answer);
     $logList.append(newList);
   }
@@ -16,6 +45,7 @@ $(document).ready(function() {
       var newList = $('<li>');
       // √ console.log($input.val());
       newList.text($input.val());
+      newList.addClass('command')
       $logList.append(newList);
       respond($input.val());
       $input.val('');
