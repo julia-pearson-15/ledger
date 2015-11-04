98ff193377c676d03870f73945ba9b65495a1cad
Julia Pearson <julia.pearson@gmail.com>
2015-11-04 15:23:45 -0500

    interest time

--- main.js
+++ main.js
@@ -2,23 +2,33 @@ $(document).ready(function() {
   var $input = $('#input');
   var $logList = $('#log');
   var purseBal = 1000;
+  var ledgerList = '';
+  var interestRate = .15;
+
 
   var makeStringArray = function() {
     var stringed = $input.val();
     var words = stringed.split(' ');
     return words;
   }
-  // var checkIfLoan = function() {
-  //   if ($input.val().charAt(0)==='l') {
-  //     if ($input.val().charAt(0)==='o') {
-  //       if ($input.val().charAt(0)==='a') {
-  //         if ($input.val().charAt(0)==='n') {
-  //           return true;
-  //         }; 
-  //       };  
-  //     };  
-  //   };
-  // }
+
+  var makeString = function(arrayTime) {
+    var toString ='';
+    for (var i = 0; i<arrayTime.length; i++) {
+      toString+=' '+arrayTime[i];
+    };
+    return toString;
+  }
+
+  var getInterest = function() {
+    var addingI = ledgerList.split(' ');
+    for (var i = 3; i<addingI.length; i=i+5) {
+      console.log(addingI[i]);
+      addingI[i]=parseInt(addingI[i])+parseInt(addingI[i])*interestRate;
+      console.log(addingI[i]);
+    };
+    return makeString(addingI);
+  }
 
   var respond = function(question){
     var answer  = '';
@@ -29,10 +39,16 @@ $(document).ready(function() {
       answer='ShylockBot pulls out his purse containing '+purseBal+' ducats';
     }else if(questions[0]==='loan'){
       newList.addClass('action');
-      answer='ShylockBot gives'+questions[1]+' '+questions[2]+' ducats';
+      answer='ShylockBot gives '+questions[1]+' '+questions[2]+' ducats';
       purseBal-=questions[2];
-    }else{
-
+      ledgerList +=questions[1]+' owes me '+questions[2]+' ducats, ';
+    }else if(questions[0]==='ledger'){
+      answer = ledgerList;
+      console.log(ledgerList);
+    }else if(questions[0]==='collect' && questions[1]==='interest'){
+      answer = getInterest();
+    }else if(questions[0]==='set'){
+      interestRate=questions[4];
     }
     newList.text(answer);
     $logList.append(newList);
